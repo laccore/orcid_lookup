ff8868ad5d0ee4488b53c5e16490c89969a69744
enderu <enderu@gmail.com>
2021-12-08 10:06:24 -0600

    add argparse, update README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# orcid_lookup
+# ORCID Lookup Tool
+
+Steps to run script:
+1. Clone/download repository
+2. Navigate to folder
+3. (Optionally) configure virtual env
+`python3 -m venv env`
+4. `pip install -r requirements.txt`
+5. `python3 orcid_lookup.py email_list.csv`
+
+The file `email_list.csv` should be a csv/text file, with one email address per row.
--- orcid_lookup.py
+++ orcid_lookup.py
@@ -1,9 +1,9 @@
+import argparse
 import csv
-from random import randint
-from time import sleep
-import chardet
 import locale
+from time import sleep
 
+import chardet
 import requests
 import untangle
 
@@ -17,57 +17,105 @@ def guess_file_encoding(input_file, verbose=False):
     return enc["encoding"]
 
 
-input_file = "emails.csv"
+def lookup_orcids(
+    email_list_filename,
+    matched_filename="matched.csv",
+    unmatched_filename="not_matched.csv",
+):
+    emails = []
+    enc = guess_file_encoding(email_list_filename)
+    with open(email_list_filename, "r", encoding=enc) as emf:
+        for r in csv.reader(emf):
+            emails.append(r[0])
 
-emails = []
-enc = guess_file_encoding(input_file)
-with open(input_file, "r", encoding=enc) as emf:
-    for r in csv.reader(emf):
-        emails.append(r[0])
+    # TODO: check emails for: 1) spaces, 2) quotation marks, 3) new lines
 
-# TODO: check emails for: 1) spaces, 2) quotation marks, 3) new lines
+    url = "https://pub.orcid.org/v3.0/search/?q=email:"
 
-url = "https://pub.orcid.org/v3.0/search/?q=email:"
+    print(f"Checking {len(emails)} email addresses for ORCIDs.")
 
-print(f"Checking {len(emails)} email addresses for ORCIDs.")
+    with open(
+        matched_filename, "a+", encoding=locale.getpreferredencoding()
+    ) as mf, open(
+        unmatched_filename, "a+", encoding=locale.getpreferredencoding()
+    ) as nmf:
+        mw = csv.writer(mf)
+        nmw = csv.writer(nmf)
 
-with open("matches.csv", "a+", encoding=locale.getpreferredencoding()) as mf, open(
-    "not_matched.csv", "a+", encoding=locale.getpreferredencoding()
-) as nmf:
-    mw = csv.writer(mf)
-    nmw = csv.writer(nmf)
+        count = 0
+        for email in emails:
+            print(f"Checking {email}...")
+            r_url = url + email
+            r = requests.get(r_url)
+            obj = untangle.parse(r.text)
 
-    count = 0
-    for email in emails:
-        print(f"Checking {email}...")
-        r_url = url + email
-        r = requests.get(r_url)
-        obj = untangle.parse(r.text)
+            try:
+                obj.search_search["num-found"]
+            except:
+                print(r.text)
+                break
 
-        try:
-            obj.search_search["num-found"]
-        except:
-            print(r.text)
-            break
+            if int(obj.search_search["num-found"]) > 0:
+                orcid = (
+                    obj.search_search.search_result.common_orcid_identifier.common_path.cdata
+                )
+                mw.writerow((email, orcid))
+                mf.flush()
+                print("\tORCID found.")
+            else:
+                nmw.writerow([email])
 
-        if int(obj.search_search["num-found"]) > 0:
-            orcid = (
-                obj.search_search.search_result.common_orcid_identifier.common_path.cdata
-            )
-            mw.writerow((email, orcid))
-            mf.flush()
-            print("\tORCID found.")
-        else:
-            nmw.writerow([email])
+            sleep(0.1)
+            count += 1
+            if (count % 100) == 0:
+                print()
+                print(
+                    f"Checked {count} email addresses. {round(count/len(emails)*100,2)}% complete."
+                )
+                print()
 
-        sleep(0.1)
-        count += 1
-        if (count % 100) == 0:
-            print()
-            print(
-                f"Checked {count} email addresses. {round(count/len(emails)*100,2)}% complete."
-            )
-            print()
+    print("Complete")
+    print(f"Checked {count} emails")
 
-print("Complete")
-print(f"Checked {count} emails")
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Check for publicly available ORCIDs associated with a list of email addreses."
+    )
+    parser.add_argument(
+        "email_list", type=str, help="Name of email input file (one record per row)."
+    )
+    parser.add_argument(
+        "-o",
+        "--output_filename_matched",
+        type=str,
+        help="Name of the output file for matched ORCIDs.",
+    )
+    parser.add_argument(
+        "-u",
+        "--output_filename_unmatched",
+        type=str,
+        help="Name of the output file for unmatched email addresses.",
+    )
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Increase verbosity."
+    )
+
+    args = parser.parse_args()
+
+    mofn = (
+        "matched.csv"
+        if not args.output_filename_matched
+        else args.output_filename_matched
+    )
+    uofn = (
+        "not_matched.csv"
+        if not args.output_filename_unmatched
+        else args.output_filename_unmatched
+    )
+
+    lookup_orcids(args.email_list, mofn, uofn)
+
+
+if __name__ == "__main__":
+    main()
